54158b6a89751be249d654f60ed7d70c835aa425
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-09 12:04:13 +0530

    removeUserCompleted

--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:login_page/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePageOne extends StatelessWidget {
+  removeValues() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //Remove String
+    prefs.remove("email");
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
             body: Center(
       child: Container(
-        child: Text(
-          'Welcome',
-          style: TextStyle(fontSize: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome',
+              style: TextStyle(fontSize: 40),
+            ),
+            RaisedButton(
+                child: Text('Logout'),
+                onPressed: () {
+                  removeValues();
+
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()));
+                })
+          ],
         ),
       ),
     )));
